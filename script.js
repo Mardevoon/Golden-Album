8eb028ed8939a7ff7e5275aff2a0018b4c8a8f0a
Roberto Souza <marcos-sf@outlook.com>
2024-09-05 16:53:16 -0300

    Acrescimo das funcionalidades: *Deixar as cartas em escala grande; *Centralizadas na tela; *Escurecer o fundo; *Fechar a imagem clicando em qualquer lugar fora da imagem.

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+const lightbox = document.querySelector('.lightbox');
+const lightboxImage = document.querySelector('.lightbox-content');
+const images = document.querySelectorAll('.carta-imagem');
+const closeButton = document.querySelector('.close');
+
+/*Comando para deixar a carta grande na tela e escurecer o fundo*/
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    lightboxImage.src = image.src;
+    lightbox.style.display = 'flex';
+  });
+});
+
+closeButton.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (event) => {
+  if (event.target === lightbox) {
+    lightbox.style.display = 'none';
+  }
+});
+
 /*Script do menu para rolagem de tela*/
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 650)
 })
+
